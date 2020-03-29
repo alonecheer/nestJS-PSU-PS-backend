8f13992978a9e13bce9d1d7981fde81423428d37
alonecheer <alonecheer@hotmail.com>
2020-03-29 17:10:12 +0700

    npm soap

--- src/userloginpsu/userloginpsu.service.ts
+++ src/userloginpsu/userloginpsu.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { Userpsu } from './userloginpsu.entity';
+import * as soap from 'soap';
+import { CreateUserDto } from './dto/create-user.dto';
 @Injectable()
 export class UserloginpsuService {
     constructor(
@@ -7,5 +9,10 @@ export class UserloginpsuService {
 
     async findAll(): Promise<Userpsu[]>{
         return this.userpsuRepository.findAll<Userpsu>();
+    }
+
+    login(CreateUserDto: CreateUserDto){
+        const PSU_URL = 'https://passport.psu.ac.th/authentication/authentication.asmx?wsdl';
+        return 
     }
 }
