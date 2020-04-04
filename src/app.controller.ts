c095049996602c6a5b9b7e59df55cba12344c10e
alonecheer <alonecheer@hotmail.com>
2020-04-04 15:27:01 +0700

    delete app service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,17 +2,12 @@ import { Controller, Request, Post, UseGuards , Get } from '@nestjs/common';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(
     private authService: AuthService,
-    private readonly appService: AppService) {}
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    ) {}
   
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,11 @@ import { AppController } from './app.controller';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { AppService } from './app.service'
 import { UserloginpsuModule } from './userloginpsu/userloginpsu.module';
 
 @Module({
   imports: [DatabaseModule,AuthModule, UsersModule, UserloginpsuModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
