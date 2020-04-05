be007af76d62db423c8202e50c1828f03adabffa
alonecheer <alonecheer@hotmail.com>
2020-04-05 23:49:12 +0700

    exports AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,8 @@ import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from 'src/user/user.module';
 import { jwtConstants } from './constants';
 import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
+import { SinginUser } from 'src/user/dto/singin-user.dto';
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
@@ -14,6 +16,7 @@ import { LocalStrategy } from './local.strategy';
     }),
     UserModule,
   ],
-  providers: [AuthService,LocalStrategy,]
+  providers: [AuthService,LocalStrategy,JwtStrategy,SinginUser],
+  exports:[AuthService],
 })
 export class AuthModule {}
