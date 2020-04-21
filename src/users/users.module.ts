31b1929ed3f4411034da9748e1742e337c3965d7
alonecheer <alonecheer@hotmail.com>
2020-04-21 19:12:52 +0700

    add provider

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { DatabaseModule } from 'src/database/database.module';
+import { usersProviders } from './users.providers';
 
 
 @Module({
   imports:[DatabaseModule],
   controllers: [UsersController],
-  providers: [UsersService,],
+  providers: [UsersService,...usersProviders],
   exports:[UsersService]
 })
 export class UsersModule {}
