005988e4dd222990f9ce1f5f3a89ed2fb4c3e2fd
alonecheer <alonecheer@hotmail.com>
2020-04-21 19:10:50 +0700

    fillAll user indatabase

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable , Inject } from '@nestjs/common';
+import { User } from './entity/users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@Inject('USERS_REPOSITORY')private user : typeof User){}
+
+    async findAll(): Promise<User[]> {
+        const user = await this.user.findAll<User>();
+        return user;
+    }
+    
+}
