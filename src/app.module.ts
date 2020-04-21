7b61f933774d739999f832d31908c9ff1b01c5a3
alonecheer <alonecheer@hotmail.com>
2020-04-21 18:53:59 +0700

    nest g m users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [],
   providers: [],
 })
