75d49a86a7a2455481e7e090e597fc4ee715e34a
alonecheer <alonecheer@hotmail.com>
2020-04-21 19:09:32 +0700

    add dto users

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,19 @@
+
+
+export class CreateUserDto{
+
+
+    username:string;
+    password:string;
+
+}
+
+export class CreateUserDetailDto{
+
+
+    sid:string;
+    firstname:string;
+    lastname:string;
+    cid:string;
+
+}
