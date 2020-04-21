7e8ae3afa69cdc97574a3e81af913e5aef37d340
alonecheer <alonecheer@hotmail.com>
2020-04-21 19:21:27 +0700

    add cunstants jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-
+import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
+  imports:[UsersModule,PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '180s' },
+    }),
+  UsersModule
+  ],
   providers: [AuthService]
 })
 export class AuthModule {}
