0de64e8148db2cda80dfe62c376c96e55199e395
alonecheer <alonecheer@hotmail.com>
2020-05-05 02:32:30 +0700

    error null model from001

--- src/form001/dto/forms.dto.ts
+++ src/form001/dto/forms.dto.ts
@@ -1,5 +1,28 @@
 export class InsertFormDto{
-    loca:string;
-    date:string;
     sid:string;
+    o_location:string;
+    o_date:string;
+    o_purpose: string;
+    o_projectname: string;
+    o_require: string;
+    o_specific : number;
+    o_appointment: number;
+    o_results: number;
+    o_committee1: string;
+    o_committee2: string;
+    o_committee3: string;
+    o_committee4: string;
+    o_committee5: string;
+    o_committee6: string;
+    o_gvm_sub: number;
+    o_income: number;
+    o_aml_income: number;
+    o_direction: string;
+    o_policy: string;
+    o_works: string;
+    o_task: string;
+    o_expense_cg: string;
+    o_sub_cg: string;
+    o_list_n: string;
+    o_other_m: number;
 }
--- src/form001/entity/forms.entity.ts
+++ src/form001/entity/forms.entity.ts
@@ -12,8 +12,8 @@ import {
 import { User } from 'src/users/entity/users.entity';
 
 @Table({
-    timestamps: false,
-    paranoid: false,
+    timestamps: true,
+    paranoid: true,
 })
 export class Form001 extends Model<Form001> {
     @PrimaryKey
@@ -22,11 +22,87 @@ export class Form001 extends Model<Form001> {
     order_id: number
     
     @Column
-    loca: string;
+    o_location: string;
 
 
     @Column
-    date: string;
+    o_date: string;
+
+    @Column
+    o_purpose: string;
+    
+    @Column
+    o_projectname: string;
+
+    @Column
+    o_require: string;
+
+    @AllowNull(true)
+    @Column
+    o_specific : number;
+
+    @AllowNull(true)
+    @Column
+    o_appointment: number;
+
+    @AllowNull(true)
+    @Column
+    o_results: number;
+
+    @Column
+    o_committee1: string;
+
+    @Column
+    o_committee2: string;
+
+    @Column
+    o_committee3: string;
+
+    @Column
+    o_committee4: string;
+
+    @Column
+    o_committee5: string;
+
+    @Column
+    o_committee6: string;
+
+    @AllowNull(true)
+    @Column
+    o_gvm_sub: number;
+
+    @AllowNull(true)
+    @Column
+    o_income: number;
+
+    @AllowNull(true)
+    @Column
+    o_aml_income: number;
+
+    @Column
+    o_direction: string;
+
+    @Column
+    o_policy: string;
+
+    @Column
+    o_works: string;
+
+    @Column
+    o_task: string;
+
+    @Column
+    o_expense_cg: string;
+
+    @Column
+    o_sub_cg: string;
+
+    @Column
+    o_list_n: string;
+
+    @AllowNull(true)
+    @Column
+    o_other_m: number;
 
     @BelongsTo(()=> User)
     user:User;
