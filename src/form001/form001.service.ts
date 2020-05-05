6fc390efafd13e8a49db7008b973f996a8cb4859
alonecheer <alonecheer@hotmail.com>
2020-05-05 17:01:33 +0700

    add attributes

--- src/form001/form001.service.ts
+++ src/form001/form001.service.ts
@@ -17,12 +17,21 @@ export class Form001Service {
 
     async getForm001Bysid(sid : string){
         const found = await this.form001.findAll({
-            attributes: ['sid','loca','date'],
+            attributes: [
+                'sid','o_location','o_date','o_purpose',
+                'o_projectname','o_require','o_specific',
+                'o_appointment','o_results','o_committee1',
+                'o_committee2','o_committee3','o_committee4',
+                'o_committee5','o_committee6','o_gvm_sub',
+                'o_income','o_aml_income','o_direction',
+                'o_policy','o_works','o_task','o_expense_cg',
+                'o_sub_cg','o_list_n','o_other_m',
+            ],
             where: {
                 sid: sid
             }
         })
-        console.log('found = xxxxxxxxxxxxxx',found)
+        //console.log('found = xxxxxxxxxxxxxx',found)
         if (!found){
             return 0;
         }
