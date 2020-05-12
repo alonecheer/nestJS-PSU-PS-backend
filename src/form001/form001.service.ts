ec662d0a1ff744da99a1c2b40f1163fe0c5b92cd
alonecheer <alonecheer@hotmail.com>
2020-05-12 19:34:56 +0700

    edit return attributes

--- src/form001/form001.service.ts
+++ src/form001/form001.service.ts
@@ -38,12 +38,26 @@ export class Form001Service {
         return found;
     }
     async getForm001Byorder_id(order_id : number){
-        const data = await this.form001.findByPk(order_id);
-        if(data){
-            console.log('Data : ',data)
-        return data; 
+        const found = await this.form001.findAll({
+            attributes: [
+                'sid','o_location','o_date','o_purpose',
+                'o_projectname','o_require','o_specific',
+                'o_appointment','o_results','o_committee1',
+                'o_committee2','o_committee3','o_committee4',
+                'o_committee5','o_committee6','o_gvm_sub',
+                'o_income','o_aml_income','o_direction',
+                'o_policy','o_works','o_task','o_expense_cg',
+                'o_sub_cg','o_list_n','o_other_m','order_id','createdAt',
+            ],
+            where: {
+                order_id: order_id
+            }
+        })
+        //console.log('found = xxxxxxxxxxxxxx',found)
+        if (!found){
+            return 0;
         }
-        return console.log('not data')
+        return found;
        
     }
 
