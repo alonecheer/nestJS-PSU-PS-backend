ea8d25f5a1c693a5ba853ff62edf16f48167afe0
alonecheer <alonecheer@hotmail.com>
2020-06-01 21:39:55 +0700

    import Status_form001

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize-typescript';
 import { User } from 'src/users/entity/users.entity';
 import { Form001 } from 'src/form001/entity/forms.entity';
 import { Typeform } from 'src/typeform/entity/typeform.entity'
-
+import { Status_form001 } from 'src/status-form001/entity/status-form001.entity'
 
 export const databaseProviders = [
   {
@@ -17,7 +17,7 @@ export const databaseProviders = [
         password: '0123456',
         database: 'nest-jwt',
       });
-      sequelize.addModels([Form001,User,Typeform]);
+      sequelize.addModels([Form001,User,Typeform,Status_form001]);
       await sequelize.sync();
       return sequelize;
     },
