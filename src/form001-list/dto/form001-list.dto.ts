ee18f30ca803b3df93f6ba868e39278486398030
alonecheer <alonecheer@hotmail.com>
2020-06-01 22:46:18 +0700

    dto

---
+++ src/form001-list/dto/form001-list.dto.ts
@@ -0,0 +1,17 @@
+export class Form001_ListDto {
+    l_id: number
+    
+
+    l_detail: string;
+
+    
+    l_amount: number;
+
+    
+    l_unit: string;
+
+    
+    l_priceunit: number;
+
+    sid: string;
+}
