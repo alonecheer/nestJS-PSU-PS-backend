5250d88016d98b0229da45b97098faa64b0e218f
alonecheer <alonecheer@hotmail.com>
2020-06-01 21:38:30 +0700

    add status module

--- src/status-form001/status-form001.module.ts
+++ src/status-form001/status-form001.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StatusForm001Controller } from './status-form001.controller';
 import { StatusForm001Service } from './status-form001.service';
-
+import { DatabaseModule } from 'src/database/database.module';
+import { Status_Form001Providers } from './status-form001.provider';
 @Module({
+  imports:[DatabaseModule],
   controllers: [StatusForm001Controller],
-  providers: [StatusForm001Service]
+  providers: [StatusForm001Service,...Status_Form001Providers]
 })
 export class StatusForm001Module {}
--- src/status-form001/status-form001.provider.ts
+++ src/status-form001/status-form001.provider.ts
@@ -1,6 +1,6 @@
 import { Status_form001 } from "./entity/status-form001.entity";
 
-export const Form001Providers = [
+export const Status_Form001Providers = [
   {
     provide: 'status_form001Repo',
     useValue: Status_form001,
