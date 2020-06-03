d655341af88328582e9238bf3a33f4cb53b64e8a
alonecheer <alonecheer@hotmail.com>
2020-06-04 03:09:37 +0700

    fix post

--- src/form001-list/dto/form001-list.dto.ts
+++ src/form001-list/dto/form001-list.dto.ts
@@ -1,10 +1,26 @@
 export class Form001_ListDto {
     l_id: number
-    l_detail: string;
-    l_amount: number;
-    l_unit: string;
-    l_priceunit: number;
-    
+    // -------------------
+    l_detail_1: string;
+    l_amount_1: number;
+    l_unit_1: string;
+    l_priceunit_1: number;
+    // -------------------
+    l_detail_2: string;
+    l_amount_2: number;
+    l_unit_2: string;
+    l_priceunit_2: number;
+    // -------------------
+    l_detail_3: string;
+    l_amount_3: number;
+    l_unit_3: string;
+    l_priceunit_3: number;
+    // -------------------
+    l_detail_4: string;
+    l_amount_4: number;
+    l_unit_4: string;
+    l_priceunit_4: number;
+    // -------------------
     sid: string;
     order_id: number
 }
