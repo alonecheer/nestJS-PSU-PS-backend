75b529995c6c4e6cbbbf66e42494c4522ba8e653
alonecheer <alonecheer@hotmail.com>
2020-06-04 02:40:15 +0700

    api get form001_2

--- src/form001-list/form001-list.controller.ts
+++ src/form001-list/form001-list.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Form001ListService } from './form001-list.service';
 
 @Controller('form001-list')
-export class Form001ListController {}
+export class Form001ListController {
+    constructor( private readonly form001_2Service : Form001ListService){}
+
+    @Get()
+    async getForm001_2(){
+        return this.form001_2Service.getForm001_2();
+    }
+}
--- src/form001-list/form001-list.service.ts
+++ src/form001-list/form001-list.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { Form001_List } from './entity/form001-list.entity';
 
 @Injectable()
-export class Form001ListService {}
+export class Form001ListService {
+    constructor(@Inject('form001_listRepo') private readonly form001_list: typeof Form001_List){}
+
+    async getForm001_2(){
+        return await this.form001_list.findAll();
+    }
+
+
+}
