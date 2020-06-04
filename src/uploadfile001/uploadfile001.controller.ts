cf10dfbe8313c0fad9f9eb42408af4e1521729c8
alonecheer <alonecheer@hotmail.com>
2020-06-04 19:34:45 +0700

    api uploadfile

--- src/uploadfile001/uploadfile001.controller.ts
+++ src/uploadfile001/uploadfile001.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseInterceptors, UploadedFiles } from '@nestjs/common';
+import { Uploadfile001Service } from './uploadfile001.service';
+import { FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('uploadfile001')
-export class Uploadfile001Controller {}
+export class Uploadfile001Controller {
+    constructor(private readonly uploadfile001Service : Uploadfile001Service){}
+
+    
+
+    @Post('upload')
+    @UseInterceptors(FilesInterceptor('image'))
+    uploadFile(@UploadedFiles() File){
+        console.log(File);
+    }
+}
