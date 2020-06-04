f1416f5c1a34016a61472d33a54fcd8687bddf11
alonecheer <alonecheer@hotmail.com>
2020-06-04 19:24:41 +0700

    import FileInterceptor , MulterModule

--- src/uploadfile001/uploadfile001.module.ts
+++ src/uploadfile001/uploadfile001.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { Uploadfile001Controller } from './uploadfile001.controller';
 import { Uploadfile001Service } from './uploadfile001.service';
-
+import { FileInterceptor, MulterModule } from '@nestjs/platform-express'
 @Module({
+  imports:[MulterModule.register({
+    dest: './uploads'
+  })],
   controllers: [Uploadfile001Controller],
   providers: [Uploadfile001Service]
 })
