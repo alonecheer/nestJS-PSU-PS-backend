2594eba1b4b7765cd69af2392c9f040c3a574d04
alonecheer <alonecheer@hotmail.com>
2020-06-04 20:32:55 +0700

    file-upload.utils.ts

--- src/uploadfile001/uploadfile001.controller.ts
+++ src/uploadfile001/uploadfile001.controller.ts
@@ -1,16 +1,23 @@
-import { Controller, Post, UseInterceptors, UploadedFiles } from '@nestjs/common';
+import { Controller, Post, UseInterceptors, UploadedFiles, Get, Res, Param } from '@nestjs/common';
 import { Uploadfile001Service } from './uploadfile001.service';
 import { FilesInterceptor } from '@nestjs/platform-express';
+import { createWriteStream } from 'fs';
 
 @Controller('uploadfile001')
 export class Uploadfile001Controller {
     constructor(private readonly uploadfile001Service : Uploadfile001Service){}
 
-    
+    @Get(':imgpath')
+    seeUploadFile(@Param('imgpath') image,@Res() res){
+        return res.sendFile(image, {root: 'uploads'});
+    }
 
     @Post('upload')
-    @UseInterceptors(FilesInterceptor('image'))
-    uploadFile(@UploadedFiles() File){
-        console.log(File);
+    @UseInterceptors(FilesInterceptor('files'))
+    uploadFile(@UploadedFiles() files){
+        console.log(files);
+
     }
+
+    
 }
