a060a2ad92a7cd356e79f177e5c4f5ed1edf9555
alonecheer <alonecheer@hotmail.com>
2020-06-05 14:24:35 +0700

    add entity to database

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -5,6 +5,7 @@ import { Form001 } from 'src/form001/entity/forms.entity';
 import { Typeform } from 'src/typeform/entity/typeform.entity'
 import { Status_form001 } from 'src/status-form001/entity/status-form001.entity'
 import { Form001_List } from 'src/form001-list/entity/form001-list.entity';
+import { Uploadfile001 } from 'src/uploadfile001/entity/uploadfile001.entity';
 export const databaseProviders = [
   {
     provide: 'SEQUELIZE',
@@ -17,7 +18,7 @@ export const databaseProviders = [
         password: '0123456',
         database: 'nest-jwt',
       });
-      sequelize.addModels([Form001,User,Typeform,Status_form001,Form001_List]);
+      sequelize.addModels([Form001,User,Typeform,Status_form001,Form001_List,Uploadfile001]);
       await sequelize.sync();
       return sequelize;
     },
