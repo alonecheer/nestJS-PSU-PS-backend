a531a6df4d5c478265c6597a32bdda43cbec0aa9
alonecheer <alonecheer@hotmail.com>
2020-06-05 14:23:14 +0700

    import database

--- src/uploadfile001/uploadfile001.module.ts
+++ src/uploadfile001/uploadfile001.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { Uploadfile001Controller } from './uploadfile001.controller';
 import { Uploadfile001Service } from './uploadfile001.service';
 import { FileInterceptor, MulterModule } from '@nestjs/platform-express'
+import { DatabaseModule } from 'src/database/database.module';
 @Module({
   imports:[MulterModule.register({
     dest: './files'
-  })],
+  }),DatabaseModule],
   controllers: [Uploadfile001Controller],
   providers: [Uploadfile001Service]
 })
