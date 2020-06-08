44267e86a4ed3879f0d99ce35379efca013df634
alonecheer <alonecheer@hotmail.com>
2020-06-08 22:53:43 +0700

    add database and provider

--- src/user-type/user-type.module.ts
+++ src/user-type/user-type.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserTypeController } from './user-type.controller';
 import { UserTypeService } from './user-type.service';
-
+import { DatabaseModule } from 'src/database/database.module';
+import { User_typeProviders } from './user-type.provider';
 @Module({
+  imports:[DatabaseModule],
   controllers: [UserTypeController],
-  providers: [UserTypeService]
+  providers: [UserTypeService,...User_typeProviders]
 })
 export class UserTypeModule {}
